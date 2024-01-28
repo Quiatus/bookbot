fe3cedd4d114f58ab79a718d08e459c1d67f5983
Quiatus <Quiatus@gmail.com>
2024-01-28 20:10:52 +0100

    add char count

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-def main():
-
+def open_book():
     with open("books/frankenstein.txt") as f:
         file_content = f.read()
-        print(file_content)
+        return file_content
+    
+def count_words():
+    book = open_book()
+    words = book.split()
+    print(f"This book has {len(words)} words")
 
+def main():
+    count_words()
+    book = open_book().lower()
+    count = {}
+
+    for char in book:
+        if char in count.keys():
+            count[char] += 1
+        else:
+            count[char] = 1
+
+    print(count)
+
+    
 main()
